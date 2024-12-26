827cad4ca16cc0de513d7bdf223389a1fd9f5a7f
INHA <167923321+INHA-JEONG@users.noreply.github.com>
2024-12-26 22:50:20 +0900

    [Bronze V] Title: A+B - 7, Time: 56 ms, Memory: 6052 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,23 @@
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int total = int.Parse(Console.ReadLine());
+            string[] ints = new string[total];
+            for (int i = 0; i < total; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int a = int.Parse(input[0]);
+                int b = int.Parse(input[1]);
+                int add = a + b;
+                ints[i] = $"Case #{i+1}: {add}";
+            }
+            foreach (var number in ints)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
