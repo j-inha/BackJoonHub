7be332f08dd11fca8617e4161ab894a3858a17f8
INHA <167923321+j-inha@users.noreply.github.com>
2025-01-09 01:20:01 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 48 ms, Memory: 5528 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cs
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cs
@@ -6,16 +6,16 @@ class Program
         bool[] students = new bool[31]; // 0번은 사용하지 않음
 
         // 과제 제출한 학생 번호 입력받기
-        for (int i = 0; i < 28; i++) // 반복문을 반복하는 횟수 0이면 <28 , 1이면 <29
+        for (int i = 1; i < 29; i++) // 반복문을 반복하는 횟수 0이면 <28 , 1이면 <29
         {
             int s = int.Parse(Console.ReadLine());
-            students[s] = true; // 제출한 학생 28명이 true로 표시
+            students[s] = true; // 제출한 학생 28명이 true 표시
         }
 
         // 제출하지 않은 학생 번호 출력
         for (int i = 1; i <= 30; i++)
         {
-            if (!students[i]) // false인 학생 번호 출력
+            if (!students[i]) // false 학생 번호 출력
             {
                 Console.WriteLine(i);
             }
