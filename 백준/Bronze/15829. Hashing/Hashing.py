1d879e110fa03dee3707be67694ae52c319a882f
INHA <167923321+j-inha@users.noreply.github.com>
2025-01-21 21:27:57 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,9 +2,11 @@ count = int(input())
 number = input()
 text = list(number)
 answer = 0
+r = 31
+M = 1234567891
 custom_mapping = {chr(i): i - 96 for i in range(97,123)}
 for i in range(count):
-    
     if  text[i] in custom_mapping:                                                                                                                 
-        answer += custom_mapping[text[i]] * 31**i
+        answer = (answer + custom_mapping[text[i]] * pow(r, i, M)) % M
+        
 print(answer)
